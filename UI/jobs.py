2d8173117bdcbb519efa02b612168d01d7f9f342
Jthew001 <jthew475@gmail.com>
2017-04-25 10:48:26 -0400

    added jobs

--- UI/jobs.py
+++ UI/jobs.py
@@ -12,6 +12,90 @@ j = {
 			"Done Early":7.0,
 		}
 	},
+        'Politican': {
+            'title': 'Politican', 
+            'income':16500,
+            'company': "USA BABY", 
+            'area': 'suburban',
+            'events':{
+                "Half-Day (Nothing new)":4.0,
+                "Average Day":8.0,
+                "Filibuster":9.0,
+                "Left early, dispite bills needing to be passed":7.0,
+                }
+        },
+        'Student': {
+            'title': 'Student', 
+            'income':3000,
+            'company': "Generic College", 
+            'area': 'urban',
+            'events':{
+                "Catch-up on school work":4.0,
+                "Average Day":8.0,
+                "Had to work overtime":9.0,
+                "Got someone to cover end of shift":7.0,
+                }
+        },
+        'Entrepreneur': {
+            'title': 'Entrepreneur', 
+            'income':17000,
+            'company': "Hometown Rescue", 
+            'area': 'urban',
+            'events':{
+                "Not Busy":4.0,
+                "Average Day":8.0,
+                "Too much to do":9.0,
+                "Need small break from work":7.0,
+                }
+        },
+        'Contractor': {
+            'title': 'Contractor', #Roofer??
+            'income':12000,
+            'company': "Ray's Roofers", 
+            'area': 'suburban',
+            'events':{
+                "Nailed Hand on Accident":4.0,
+                "Average Day":8.0,
+                "Productive Day":9.0,
+                "Need break from heat":7.0,
+                }
+        },
+        'Uber Driver': {
+            'title': 'Uber Driver',
+            'income':4000,
+            'company': "Uber", 
+            'area': 'urban',
+            'events':{
+                "Car broke down":4.0,
+                "Average Day":8.0,
+                "Long Trip":9.0,
+                "Taking it easy":7.0,
+                }
+        },
+        'Fishermen': {
+            'title': 'Fishermen',
+            'income':7000,
+            'company': "Ocean Abusers", 
+            'area': 'rural',
+            'events':{
+                "Capsized":4.0,
+                "Average Catch":8.0,
+                "Stuck on the seas":9.0,
+                "Caught quota":7.0,
+                }
+        },
+        'Programmer': {
+            'title': 'Programmer',
+            'income':15000,
+            'company': "Real Koders", #I have no idea for name
+            'area': 'urban',
+            'events':{
+                "Honest about time actually worked":4.0,
+                "Easy Day":8.0,
+                "Have to meet deadline":9.0,
+                "Done Early":7.0,
+                }
+        },
 	'Plumber': {
             'title': 'Plumber',
             'income':10000,
